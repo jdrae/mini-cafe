91706075c2d0d3e0e57bfe23c98688a1005c824c
jdrae <draejang@gmail.com>
2022-06-17 21:52:19 +0900

    set 'user_id' cookie when logged in

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,26 @@
 from django.contrib.auth import login
 from django.shortcuts import redirect
 from django.views import generic
+from django.contrib.auth import views as auth_views
 
 from .forms import UserForm
 from .models import User
+
+
+class LoginView(auth_views.LoginView):
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        response.set_cookie("user_id", self.request.user.id)
+        return response
+
+
+class LogoutView(auth_views.LogoutView):
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        response.delete_cookie("user_id")
+        return response
 
 
 class SignUpView(generic.CreateView):
@@ -14,4 +31,7 @@ class SignUpView(generic.CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('/')
+
+        response = redirect('/')
+        response.set_cookie("user_id", self.request.user.id)
+        return response
